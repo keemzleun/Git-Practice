2bbf5cc91a0b58e9819518230d9c6e7e6742090b
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-05-28 20:21:51 +0900

    1.env_setting

--- basic_syntax/1.env_setting.sh
+++ basic_syntax/1.env_setting.sh
@@ -6,5 +6,12 @@
 git init
 
 # 원격지 주소를 생성 및 추가
-# 원격지 주소 추가
-git remote add origin 레포 주소
+# 원격지 주소 추가     * origin은 항상 원격을 의미
+git remote add origin 레포주소
+# 원격지 주소 삭제
+git remote remove origin
+# 원격지 주소 변경
+git remote set-url origin 레포주소
+
+# git 설정 정보 조회
+git config --list
