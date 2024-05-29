c91470c8eed9c78aeaf0f3febf9b058b482ac00c
keemzleun <jin9ni0120@naver.com>
2024-05-29 15:04:06 +0900

    jijijihjihihi

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -7,7 +7,10 @@ git add .
 # 특정 파일만 add
 git add 특정파일
 
-
+# local repository에 반영 및 커밋 이력 생성
+git commit -m "메세지 타이틀" -m "메시지 내용"
+# vi 모드에서 작성 : git commit 후 enter
+git commit 
 # commit 이력 확인 (local repository의 이력)
 git log
 
