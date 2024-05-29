5ff2e0be684bf4be49db88894bafc2545db84399
keemzleun <jin9ni0120@naver.com>
2024-05-29 15:08:27 +0900

    동시에 하기

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -9,8 +9,11 @@ git add 특정파일
 
 # local repository에 반영 및 커밋 이력 생성
 git commit -m "메세지 타이틀" -m "메시지 내용"
+
 # vi 모드에서 작성 : git commit 후 enter
-git commit 
+
+# add와 commit을 동시에
+git commit -am "add와 commit을 동시에"
+
 # commit 이력 확인 (local repository의 이력)
 git log
-
